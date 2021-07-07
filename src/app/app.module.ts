597bc182a66676ece11152249151f43d66ad9f94
Selenuix <anthony.ciszek@outlook.com>
2021-07-07 11:28:47 +0200

    Add: Contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
